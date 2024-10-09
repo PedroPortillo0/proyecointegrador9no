abc31dbb9652bd60d66c499365d80ca7d166ea33
PedroPortillo0 <pedro.portillo.r22@gmail.com>
2024-10-08 18:34:04 -0600

    [UPDATE] recomposicion de estructura infraestructure/routes

--- src/payment/infraestructure/routes/paymentRoutes.ts
+++ src/payment/infraestructure/routes/paymentRoutes.ts
@@ -1,12 +1,15 @@
 import express, { Request, Response } from 'express';
-import { createPayment, getPaymentStatus } from '../../aplication/CreatePayment';
+import { CreatePayment } from '../../application/CreatePayment';
+import { mercadoPagoClient } from '../mercadoPago/MercadoPagoClient';
 
 const router = express.Router();
+
+const createPaymentUseCase = new CreatePayment(mercadoPagoClient);
 
 router.post('/create-payment', async (req: Request, res: Response) => {
   try {
     const { items, email } = req.body;
-    const paymentResponse = await createPayment(items, email);
+    const paymentResponse = await createPaymentUseCase.execute(items, email);
     res.status(200).json(paymentResponse);
   } catch (error) {
     console.error('Error al crear el pago:', error);
@@ -17,7 +20,7 @@ router.post('/create-payment', async (req: Request, res: Response) => {
 router.get('/payment-status/:id', async (req: Request, res: Response) => {
   try {
     const paymentId = req.params.id;
-    const paymentStatus = await getPaymentStatus(paymentId);
+    const paymentStatus = await mercadoPagoClient.getPaymentStatus(paymentId);
     res.status(200).json(paymentStatus);
   } catch (error) {
     console.error('Error al obtener el estado del pago:', error);
