451c82f4662064c609948aac703894320a687218
PedroPortillo0 <pedro.portillo.r22@gmail.com>
2024-10-08 18:21:42 -0600

    [UPDATE] recomposicion de la estructura domain

--- src/payment/domain/Payment.ts
+++ src/payment/domain/Payment.ts
@@ -1,28 +1,7 @@
-import express, { Request, Response } from 'express';
-import { createPayment, getPaymentStatus } from '../aplication/CreatePayment'; 
-
-const router = express.Router();
-
-router.post('/create-payment', async (req: Request, res: Response) => {
-  try {
-    const { items, email } = req.body;
-    const paymentResponse = await createPayment(items, email);
-    res.status(200).json(paymentResponse);
-  } catch (error) {
-    console.error('Error al crear el pago:', error);
-    res.status(500).json({ error: (error instanceof Error) ? error.message : 'Error desconocido' });
-  }
-});
-
-router.get('/payment-status/:id', async (req: Request, res: Response) => {
-  try {
-    const paymentId = req.params.id;
-    const paymentStatus = await getPaymentStatus(paymentId);
-    res.status(200).json(paymentStatus);
-  } catch (error) {
-    console.error('Error al obtener el estado del pago:', error);
-    res.status(500).json({ error: (error instanceof Error) ? error.message : 'Error desconocido' });
-  }
-});
-
-export default router;
+export interface Payment {
+  id: string;
+  status: string;
+  amount: number;
+  email: string;
+  external_reference: string;
+}
