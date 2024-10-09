27f37d549724239febafdf35156be92336740cc7
PedroPortillo0 <pedro.portillo.r22@gmail.com>
2024-10-08 18:57:57 -0600

    [UPDATE] controllers

--- src/payment/infraestructure/routes/paymentRoutes.ts
+++ src/payment/infraestructure/routes/paymentRoutes.ts
@@ -1,31 +1,9 @@
-import express, { Request, Response } from 'express';
-import { CreatePayment } from '../../application/CreatePayment';
-import { mercadoPagoClient } from '../mercadoPago/MercadoPagoClient';
+import express from 'express';
+import { PaymentController } from '../controllers/PaymentController';
 
 const router = express.Router();
 
-const createPaymentUseCase = new CreatePayment(mercadoPagoClient);
-
-router.post('/create-payment', async (req: Request, res: Response) => {
-  try {
-    const { items, email } = req.body;
-    const paymentResponse = await createPaymentUseCase.execute(items, email);
-    res.status(200).json(paymentResponse);
-  } catch (error) {
-    console.error('Error al crear el pago:', error);
-    res.status(500).json({ error: (error instanceof Error) ? error.message : 'Error desconocido' });
-  }
-});
-
-router.get('/payment-status/:id', async (req: Request, res: Response) => {
-  try {
-    const paymentId = req.params.id;
-    const paymentStatus = await mercadoPagoClient.getPaymentStatus(paymentId);
-    res.status(200).json(paymentStatus);
-  } catch (error) {
-    console.error('Error al obtener el estado del pago:', error);
-    res.status(500).json({ error: (error instanceof Error) ? error.message : 'Error desconocido' });
-  }
-});
+router.post('/create-payment', PaymentController.createPayment);
+router.get('/payment-status/:id', PaymentController.getPaymentStatus);
 
 export default router;
