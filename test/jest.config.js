1bcdddff2340e422bd76cf92d0d03a15c763474a
PedroPortillo0 <pedro.portillo.r22@gmail.com>
2024-10-11 13:38:54 -0600

    prueba1

--- test/jest.config.js
+++ test/jest.config.js
@@ -2,7 +2,8 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   transform: {
-    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.(ts|tsx)$': 'ts-jest', // Usa ts-jest para archivos TypeScript
+    '^.+\\.(js|jsx)$': 'babel-jest', // Usa babel-jest para archivos JavaScript
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   globals: {
