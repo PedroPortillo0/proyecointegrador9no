f2db5dffa76d3a588f2cc4934d3cb90f254d42a3
PedroPortillo0 <pedro.portillo.r22@gmail.com>
2024-10-11 21:50:17 -0600

    prueba unitaria 3

--- test/jest.config.js
+++ test/jest.config.js
@@ -2,12 +2,12 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest', 
+    '^.+\\.tsx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   globals: {
     'ts-jest': {
-      tsconfig: 'tsconfig.json', 
+      tsconfig: 'tsconfig.json',
     },
   },
 };
